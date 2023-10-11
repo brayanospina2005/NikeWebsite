b5abd7411503b1b9549a98cc15590c7de005d2d1
brayanospina2005 <ospinabrayan431@gmail.com>
2023-10-11 12:40:44 -0500

    Nav, menu responsive

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+/* show menu */
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close');
+
+        if (navToggle && navClose) {
+            navToggle.addEventListener('click', () => navMenu.classList.toggle('show-menu'));
+            navClose.addEventListener('click', () => navMenu.classList.toggle('show-menu'));
+          }; //
+          
+/* remove menu mobile */
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = ( ) => {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
